96c87a9f1639265977219579c597e95bd8bbf360
Diego Valles <dgvall134@gmail.com>
2022-10-02 18:53:05 -0600

    commit tree d5730a0f31342c4d894e5bd27c188ba7bba8b187

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+if (word === reverseWord(word)) {
+  return true
+} else return false
+}
+
+function reverseWord(word) {
+  let string = word
+  let array = string.split(``)
+  array.reverse()
+  return array.join(``)
 }
 
 /* 
   Add your pseudocode here
+
+  function isPalindrome(word){
+
+  if word === reverseWord(word) {
+    return true
+  } else return false
+  }
+
+  reverseWord(word) {
+    segment word by character and put into array
+    reverse array
+    convert array to word
+  }
+
+ 
 */
 
 /*
   Add written explanation of your solution here
+   isPalindrome will take a word. Word will be called into a function "reverseWord"
+   and tested against the original word. If they are equal it will return true, if not
+   it will return false. Function reverseWord() will create an array with the characters
+   from the word, reverse, and then convert back to a word.
 */
 
 // You can run `node index.js` to view these console logs
